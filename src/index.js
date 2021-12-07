bbe47c7ae2b1b620984eeb3b5573228580a9f264
rafael.silva <rafael.silva@tecnorisk.com.br>
2021-12-07 09:49:02 -0300

    finance project

--- src/index.js
+++ src/index.js
@@ -1,54 +1,135 @@
+const { response, request } = require('express');
 const express = require('express');
+
+const { v4: uuidv4 } = require('uuid')
 
 const app = express();
 
+
+//Middleware é a validação
+
+
+const customers = [];
 //Utilizar esse app.use para dizer vai utilizar dos envios com formato JSON
+//MIDDLEWARE PARA RECEBER EM JSON
 app.use(express.json());
 
 /**
- * GET - Buscar informação 
- * POST - Inserir informação
- * PUT - Atualizar informação
- * Patch - Atualizar informação especifica
- * DELETE - Deletar
+ * cpf = string.format
+ * name = string.format
+ * id = uuid
+ * statement []
  */
 
-/**
- * Tipos de parametros
- * 
- * Route Params => Identificar um recurso para editar ,deletar ou buscar (passar id :id) OBRIGATORIO
- * Query Params => Paginação / Filtro (?page=1&order=asc) OPCIONAL
- * Body Params => Os objetos inserção/alteração (JSON)
- */
+//Middleware
 
-app.get('/courses', (request, response) => {
+function veriftIfExistsAccountCPF(request,response, next){
+  
+    const { cpf } = request.headers;
 
-    const query = request.query;
-    console.log(query);
-    return response.json(['curso 1', 'curso 2', 'curso 3']);
+    const customer = customers.find((customer) => customer.cpf === cpf);
+   
+    if(!customer){
+        return response.status(400).json({error: "Customer Not found"});
+    }
+
+    //Para passar os valores da middleware para as rotas, criar o objeto que vai dosponibilzar os valores
+    request.customer = customer;
+
+    return next();
+
+}
+
+function getBalance(statement){
+
+    const balance = statement.reduce((acc,operation) =>{
+
+        if(operation.type === 'credit'){
+            return acc+ operation.amount;
+
+        }else{
+
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance;
+}
+
+
+app.post('/account' , (request , response)=>{
+    const {cpf, name} = request.body;
+    const id = uuidv4();
+
+    //utilizar some apenas para validação true or false
+    const customersAlreadyExist = customers.some((customer) => customer.cpf === cpf);
+
+    if(customersAlreadyExist){
+        return response.status(400).json({error: "Customer already exists!"});
+    }
+
+    customers.push(
+        {
+            cpf,
+            name,
+            id: uuidv4(),
+            statement: []
+        }
+    );
+
+    return response.status(201).send();
+});
+
+// app.use(veriftIfExistsAccountCPF);
+//para utilizar a middleware incluir a middleware no centro  da roteirização
+app.get('/statement', veriftIfExistsAccountCPF ,(request, response) => {
+
+    const { customer } = request;
+
+    return response.json(customer.statement);
+});
+
+app.post('/deposit', veriftIfExistsAccountCPF, (request,response)=> {
+
+    const {description, amount} = request.body;
+
+    const {customer} = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+
 
 });
 
-app.post('/courses', (request, response) => {
-    const body = request.body;
-    console.log(body);
-    return response.json(['curso 1', 'curso 2', 'curso 3', 'curso 4']);
-});
+app.post('/withdraw', veriftIfExistsAccountCPF, (request,response) => {
 
-app.put('/courses/:id', (request, response) => {
-    const params = request.params;
-    console.log(params);
-    return response.json(['curso 6', 'curso 2', 'curso 3', 'curso 4']);
+   const { amount } = request.body;
+   const { customer } = request;
 
-});
+   const balance = getBalance(customer.statement);
+   console.log(balance);
 
-app.patch('/courses/:id', (request, response) => {
-    return response.json(['curso 6', 'curso 2', 'curso 3', 'curso 4']);
+   if(balance < amount) {
+       return response.status(400).json({error: "Amount more than u have"});
+   }
 
-});
+   const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit"
+}
 
-app.delete('/courses/:id', (request, response)=>{
-    return response.json(['curso 6', 'curso 2', 'curso 4']);
+customer.statement.push(statementOperation);
+return response.status(201).send();
+
 });
 
 app.listen(3333);
