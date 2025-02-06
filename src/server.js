5413599f0831f22db6a24ca78b3f1138aeabb938
Rafael Silva <rafael.silva@tecnorisk.com.br>
2025-02-06 17:31:33 -0300

    up

--- src/server.js
+++ src/server.js
@@ -1,17 +1,23 @@
 import http from "http";
 
+const users = [];
+
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
   if (method === "GET" && url === "/users") {
-    return res.end("Listagem");
+    return res
+      .writeHead(200)
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
+    users.push({ id: 1, name: "John Doe", email: "email@" });
     return res.end("Criar");
   }
 
-  return res.end("Hello World");
+  return res.writeHead(404).end();
 });
 
 server.listen(3333);
